ed76af3849959fea4d701240c6e8dbefc0ef95ad
wodrow <wodrow451611cv@gmail.com>
2016-01-16 18:24:19 +0800

    trade update

--- Application/Admin/Controller/FinanceController.class.php
+++ Application/Admin/Controller/FinanceController.class.php
@@ -16,8 +16,32 @@ class FinanceController extends AdminController
         $this->display('financeIndex');
     }
 
+    /**
+     * 获取付款总额
+     * @param $map
+     * @return int
+     */
+    private function getSuccessPaymentByDate($map)
+    {
+        $trades = D("Trade")->where($map)->field("amount")->select();
+        $amount_total = 0;
+        foreach($trades as $k => $v){
+            $amount_total += $v['amount'];
+        }
+        return $amount_total;
+    }
+
+    /**
+     * 付款总额
+     */
     public function paymentTotal()
     {
+        $map['paytime'] = $this->mapDateRange;
+        $map['status'] = ['in','2,3,4'];
+        $amount_total = $this->getSuccessPaymentByDate($map);
+        $today_start = strtotime(date("Y-m-d",time())." 00:00:00");
+        $today_amount_total = $this->getSuccessPaymentByDate(['paytime'=>[['lt',$today_start]]]);
+        $this->assign(['amount_total'=>$amount_total,'today_amount_total'=>$today_amount_total]);
         $this->display();
     }
 }
--- Application/Common/Controller/CommonController.class.php
+++ Application/Common/Controller/CommonController.class.php
@@ -8,10 +8,12 @@ use Think\Controller;
  *
  * @author wodrow
  */
-abstract class CommonController extends Controller{
-    
+abstract class CommonController extends Controller
+{
+
     protected $date_start; // 获取公用开始时间
     protected $date_end;   // 获取公用结束时间
+    protected $mapDateRange; // 获取公用查询日期段
 
     /**
      *  公用查询时间
@@ -21,16 +23,17 @@ abstract class CommonController extends Controller{
         if (I('date_start') && I('date_end')) {
             $this->date_start = strtotime(I('date_start'));
             $this->date_end = strtotime(I('date_end'));
-
-	    $this->assign(['date_start' => $this->date_start,'date_end' => $this->date_end]);	
-	}else{
-	    $curr_date = date('Y');
-	    $this->date_start = strtotime($curr_date ."-1-1 00:00:00");
-	    $this->date_end = strtotime($curr_date ."-12-31 23:59:59");
-	}
+        } else {
+            $curr_date = date('Y');
+            $this->date_start = strtotime($curr_date . "-01-01 00:00:00");
+            $this->date_end = time();
+        }
+        $this->assign(['date_start' => $this->date_start, 'date_end' => $this->date_end]);
+        $this->mapDateRange = [['gt',$this->date_start],['lt',$this->date_end]];
     }
-    
-    public function _empty(){
+
+    public function _empty()
+    {
         $this->display('Public:Error');
     }
 }
