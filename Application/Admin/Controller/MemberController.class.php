cff7a88726dc143ab4cc095531d15550d11b8981
Administrator <Administrator@192.168.0.8>
2016-01-18 17:59:42 +0800

    update

--- Application/Admin/Controller/MemberController.class.php
+++ Application/Admin/Controller/MemberController.class.php
@@ -15,29 +15,32 @@ class MemberController extends AuthController{
     	$this->display();
     }
     
-    public function memberReg(){
-		
+    //会员月注册趋势
+    public function memberReg(){		
 	$Member = D('Member');
-        $mouth_solt = get_mouth_solt($this->date_start,$this->date_end);
-	
-	
-	
+        $mouth_solt = get_mouth_solt($this->date_start,$this->date_end);		
         foreach($mouth_solt as $k => $v){
             $map['regtime'] = [['gt', $v['start']['ts']], ['lt', $v['end']['ts']]];
-            $mouth_solt_trades[$k]['mouth_solt'] = $v;
-	    
-            //$mouth_solt_trades[$k]['trades'] = $Member->field('regtime')->where($map)->select();
-	    
-//            $mouth_solt_trades[$k]['mouth_name'] = date("Y-m", $v['start']['ts']);
-//            $mouth_solt_trades[$k]['trade_amount'] = get_arr_k_amount($mouth_solt_trades[$k]['trades'],'regtime');
-//            unset($mouth_solt_trades[$k]['trades']);
-	    
-	   dump($v);
-	    
-        } 
-        $this->assign('mouth_solt_trades',$mouth_solt_trades);
-        $this->display();
-	
-    }
+            $mouth_solt_member[$k]['mouth_sort'] = $v;	    
+            $mouth_solt_member[$k]['member'] = $Member->field('regtime')->where($map)->select();	    
+            $mouth_solt_member[$k]['mouth_name'] = date("Y-m", $v['start']['ts']);	    
+            $mouth_solt_member[$k]['member_amount'] = count($mouth_solt_member[$k]['member']);	    
+            unset($mouth_solt_member[$k]['member']);	    
+        }	
+	//数组处理
+	foreach($mouth_solt_member as $k => $v){
+	    $mouth_name[] = $v["mouth_name"];
+	    $member_amount[] = $v["member_amount"];
+	}
+	$mouth_name = implode("','",$mouth_name);
+	$member_amount = implode(",",str_replace(0,'',$member_amount));
+		
+	$this->assign(['mouth_name'=>$mouth_name,'member_amount'=>$member_amount]);
+        $this->display();	
+    }    
     
+    //会员信息
+    public function memberPay(){	
+	$this->display();
+    }
 }
--- Application/Admin/Controller/TradeController.class.php
+++ Application/Admin/Controller/TradeController.class.php
@@ -237,14 +237,15 @@ class TradeController extends AdminController
      * 年趋势图
      * @author iredbaby
      */
-    public function orderYearTrend(){		
+    public function orderYearTrend(){	
+	$Trade = D('Trade');
 	$year = time2year($this->year_start,$this->year_end);
 	for($i = $year['year']['start']; $i <= $year['year']['end'];$i++){	    
 	    $year_start = strtotime($i . "-01-01 00:00:00");
 	    $year_start_end = strtotime($i . "-12-31 23:59:59");	    
 	    $map['addtime'] = [['gt',$year_start],['lt',$year_start_end]];
 	    $map['status'] = ['in','2,3,4'];	    
-	    $data = D('Trade')->where($map)->field("addtime")->count();
+	    $data = $Trade->where($map)->field("addtime")->count();
 	    $yeartrade_names[] = $i;
 	    $yeartrade_total[] = $data;
 	}	
