69db3d825700489b7865b520fc5387ea814e7d3b
Edwin <junqianhen@gmail.com>
2016-03-07 18:11:22 +0800

    modify information

--- Application/Admin/Controller/InformationController.class.php
+++ Application/Admin/Controller/InformationController.class.php
@@ -18,27 +18,33 @@ class InformationController extends AdminController
         $this->display('information_index');
     }
 
-
-
+    /**
+     * 获取资讯日期
+     */
+    public function getInformationDate($map)
+    {
+        $informationDate = D("Information")->where($map)->field("addtime")->select();
+        return get_arr_k_amount($informationDate,'addtime');
+    }
 
     private function getMouthSoltInformation($date_start,$date_end)
     {
-        $Trade = D('Information');
+        $Information = D('Information');
         $mouth_solt = get_mouth_solt($date_start,$date_end);
         $map['status'] = ['in','2,3,4'];
         foreach($mouth_solt as $k => $v){
             $map['paytime'] = [['gt', $v['start']['ts']], ['lt', $v['end']['ts']]];
             $mouth_solt_information[$k]['mouth_solt'] = $v;
-            $mouth_solt_information[$k]['information'] = $Trade->field('itemid,amount')->where($map)->select();
+            $mouth_solt_information[$k]['information'] = $Information->field('itemid,content')->where($map)->select();
             $mouth_solt_information[$k]['mouth_name'] = date("Y-m", $v['start']['ts']);
-            $mouth_solt_information[$k]['trade_information'] = get_arr_k_amount($mouth_solt_information[$k]['information'],'amount');
-            unset($mouth_solt_information[$k]['information']);
+            $mouth_solt_information[$k]['trade_information'] = get_arr_k_amount($mouth_solt_information[$k]['information'],'information');
+            unset($mouth_solt_information[$k]['trades']);
         }
         return $mouth_solt_information;
     }
 
     /**
-     * 月付款
+     * 月资讯总数
      * @author wodrow
      */
     public function monthlyInformation()
