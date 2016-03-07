67479e600c0b999a4a9577f20cf652cb6b73e721
Edwin <junqianhen@gmail.com>
2016-03-07 17:01:32 +0800

    modify information

--- Application/Admin/Controller/InformationController.class.php
+++ Application/Admin/Controller/InformationController.class.php
@@ -17,12 +17,35 @@ class InformationController extends AdminController
     {
         $this->display('information_index');
     }
+
     /**
-     * 月度资讯
+     * 获取资讯日期
      */
+    public function getInformationDate($map)
+    {
+        $informationDate = D("Information")->where($map)->field("addtime")->select();
+        return get_arr_k_amount($informationDate,'addtime');
+    }
+
+    /**
+     * 资讯年总量
+     * @Edwin
+     */
+
     public function monthlyInformation()
     {
-        $count = D('Information')->field("count(itemid) AS count")->select();
-        echo $count[0]['count'];
+        $informationCount = D('Information');
+        $year_solt = get_year_solt($this->year_start,$this->year_end);
+        $map['status'] = ['in','2,3,4'];
+        foreach($year_solt as $k => $v){
+            $map['addtime'] = [['gt', $v['start']['ts']], ['lt', $v['end']['ts']]];
+            $year_solt_information[$k]['year_solt'] = $v;
+            $year_solt_information[$k]['information'] = $informationCount->field('count(addtime)')->where($map)->select();
+            $year_solt_information[$k]['year_name'] = date("Y", $v['start']['ts']);
+            $year_solt_information[$k]['infromation_amount'] = get_arr_k_amount($year_solt_information[$k]['information'],'amount');
+            unset($year_solt_information[$k]['information']);
+        }
+        $this->assign('year_solt_trades',$year_solt_information);
+        $this->display();
     }
 }
