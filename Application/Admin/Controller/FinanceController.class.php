a511a2a4bbd3d009d9ac9d6572973e0328bbfd38
Iredbaby <595697511@qq.com>
2016-03-28 17:55:40 +0800

    update

--- Application/Admin/Controller/FinanceController.class.php
+++ Application/Admin/Controller/FinanceController.class.php
@@ -67,7 +67,7 @@ class FinanceController extends AdminController
     public function mouthSoltPayment()
     {
         $this->assign('mouth_solt_trades',$this->getMouthSoltPayment($this->month_start,$this->month_end));
-        $this->display();
+
     }
 
     /**
